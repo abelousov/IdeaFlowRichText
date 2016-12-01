ac384c9556865597246fd3742020b932f5751698
Anton Belousov <anton.n.belousov@gmail.com>
2016-12-01 09:05:44 +0300

    renamings

---
+++ completionPluginSetFactory/index.js
@@ -0,0 +1,49 @@
+import createSuggestionPlugin from './plugin/createSuggestionPlugin'
+import React, {Component, PropTypes} from 'react';
+import SuggestionComponentList from './SuggestionComponentList'
+export default {
+  create (completionDescriptions) {
+    return new CompletionPluginSet(completionDescriptions)
+  }
+}
+
+class CompletionPluginSet {
+  constructor (completionDescriptors) {
+    this._createCompletionPlugins(completionDescriptors);
+  }
+
+  _createCompletionPlugins (completionDescriptors) {
+    this._completionDescriptorsByPrefixes = {}
+    this._pluginInstances = []
+
+    completionDescriptors.map((description) => {
+      const suggestionPrefix = description.prefix;
+
+      const pluginInstance = createSuggestionPlugin({suggestionRegex: this._getSuggestionRegexForPrefix(suggestionPrefix)});
+      this._pluginInstances.push(pluginInstance)
+
+      this._completionDescriptorsByPrefixes[suggestionPrefix] = {
+        SuggestionComponent: pluginInstance.CompletionSuggestions,
+        allSuggestions: description.suggestions
+      }
+    })
+  }
+
+  renderSuggestionComponents () {
+    return <SuggestionComponentList
+      descriptorsByPrefixes={this._completionDescriptorsByPrefixes}
+    />
+  }
+
+  getEditorPluginInsances () {
+    return this._pluginInstances
+  }
+
+  formatCompletions (editorState) {
+    return editorState
+  }
+
+  _getSuggestionRegexForPrefix (suggestionPrefix) {
+    return new RegExp(`\\B${suggestionPrefix}.*`, 'g')
+  }
+}
