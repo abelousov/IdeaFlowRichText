41c1a7720ce613c8f228b8312a92b648fea71b7d
Anton Belousov <anton.n.belousov@gmail.com>
2016-12-02 20:34:25 +0300

    fix autocomplete to include word part after cursor/selection start

--- src/completionSet/findCurrentAutocomplete.js
+++ src/completionSet/findCurrentAutocomplete.js
@@ -1,6 +1,6 @@
 export default function createFinder (possiblePrefixes) {
   return function (editorState) {
-    return findCurrentAutocomplete (editorState, possiblePrefixes)
+    return findCurrentAutocomplete(editorState, possiblePrefixes)
   }
 }
 
@@ -11,38 +11,53 @@ function findCurrentAutocomplete (editorState, possiblePrefixes) {
   const currentContentState = editorState.getCurrentContent()
   const currentBlock = currentContentState.getBlockForKey(currentSelection.getStartKey())
 
-  const blockText = currentBlock.getText()
+  const wholeText = currentBlock.getText()
 
-  const selectionStart = currentSelection.getStartOffset();
-  const textEndingWithSelection = blockText.substring(0, selectionStart)
+  const selectionStartIndex = currentSelection.getStartOffset();
+  const textBeforeSelectionStart = wholeText.substring(0, selectionStartIndex)
 
   let lastPrefixBeforeCurrentSelection = null
-  let lastPrefixIndex = null
+  let lastPrefixStartIndex = null
 
   possiblePrefixes.map((currentPrefix) => {
-    let currentPrefixIndex = textEndingWithSelection.lastIndexOf(currentPrefix)
+    const currentPrefixIndex = textBeforeSelectionStart.lastIndexOf(currentPrefix)
 
     if (currentPrefixIndex >= 0) {
-      if (lastPrefixIndex === null || lastPrefixIndex < currentPrefixIndex) {
-        lastPrefixIndex = currentPrefixIndex
+      if (lastPrefixStartIndex === null || lastPrefixStartIndex < currentPrefixIndex) {
+        lastPrefixStartIndex = currentPrefixIndex
         lastPrefixBeforeCurrentSelection = currentPrefix
       }
     }
   })
 
-  let textAfterLastPrefix = lastPrefixBeforeCurrentSelection
-    ? textEndingWithSelection.substring(lastPrefixIndex + lastPrefixBeforeCurrentSelection.length)
-    : null
+  if (!lastPrefixBeforeCurrentSelection) {
+    return null
+  }
 
+  const lastPrefixEndIndex = lastPrefixStartIndex + lastPrefixBeforeCurrentSelection.length;
+  const textBetweenLastPrefixAndSelectionStart = textBeforeSelectionStart.substring(lastPrefixEndIndex)
 
-  return lastPrefixBeforeCurrentSelection
-    ? {
-      prefix: lastPrefixBeforeCurrentSelection,
-      searchValueWithoutPrefix: textAfterLastPrefix,
-      selection: currentSelection.merge({
-        anchorOffset: lastPrefixIndex,
-        focusOffset: selectionStart
-      })
-    }
-    : null
+  const textAfterSelectionStart = wholeText.substring(selectionStartIndex)
+  const wordPartAfterSelectionStart = textAfterSelectionStart.match(/^\w*/)[0]
+  const wordPartBeforeSelectionStart = textBeforeSelectionStart.match(/\w*$/)[0]
+
+  const selectionStartIsInsideWord = wordPartBeforeSelectionStart.length > 0 && wordPartAfterSelectionStart.length > 0
+
+  const autocompleteStartIndex = lastPrefixEndIndex
+  let autocompleteEndIndex = selectionStartIndex
+
+  if (selectionStartIsInsideWord) {
+    autocompleteEndIndex += wordPartAfterSelectionStart.length
+  }
+
+  const autocompleteValue = wholeText.substring(autocompleteStartIndex, autocompleteEndIndex)
+
+  return {
+    prefix: lastPrefixBeforeCurrentSelection,
+    searchValueWithoutPrefix: autocompleteValue,
+    selection: currentSelection.merge({
+      anchorOffset: autocompleteStartIndex,
+      focusOffset: autocompleteEndIndex
+    })
+  }
 }
